f2a398d01cca411f2094d06c2c8770bef0c844fd
nikolai-skor <nikolaiskor@yandex.ru>
2023-02-27 17:34:06 +0300

    ФÐФоматирует докуизмененные файлы и исправляет ошибки проверки lint.

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,17 @@
 /* eslint-disable prefer-const */
 // Функция для проверки длины строки.
+
+
 const isValidate = function (sentence, lengthOfSentence) {
   return sentence.length <= lengthOfSentence;
 };
 
 isValidate('Функция для проверки', 20);
+
+
 // Функция для проверки, является ли строка палиндромом.
+
+
 const isPalindrome = function (sentence) {
   let rightDirection = [];
   let leftDirection = [];
@@ -29,14 +35,14 @@ isPalindrome('Лёша на полке клопа нашёл ');
 
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+
 const getNumber = function (stringOrNumber) {
   stringOrNumber = String(stringOrNumber);
-  console.log(stringOrNumber);
   let numberOfString = [];
   for (let i = 0; i < stringOrNumber.length; i++) {
     if (parseInt(stringOrNumber[i], 10) <= 9) {
       numberOfString.push(Number(stringOrNumber[i]));
-      console.log(numberOfString);
     }
   }
   return numberOfString.length === 0 ? NaN : Number(numberOfString.join(''));
@@ -44,24 +50,24 @@ const getNumber = function (stringOrNumber) {
 getNumber('2023 год');
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и
-// возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки.
-// Если исходная строка превышает заданную длину, она не должна обрезаться.
-// Если «добивка» слишком длинная, она обрезается с конца.
+// возвращает исходную строку, дополненную указанными символами до заданной длины.
+
+
 const createFileAddress = function (originString, minLength, extraString) {
   let newString = [];
   let partString = [];
   if (originString.length >= minLength) {
-    return newString[0] = originString;
+    return originString;
   } else {
     let t = 0;
     for (let i = 0; i < (minLength - originString.length); i++) {
       partString.push(extraString[t]);
 
-      console.log(newString);
       if (t === extraString.length - 1 && newString.join('').length + originString.length + partString.join('').length <= minLength) {
         newString.unshift(partString.join(''));
+        partString.length = 0;
         t = 0;
-              } else {
+      } else if (newString.join('').length + originString.length + partString.join('').length <= minLength) {
         t++;
       }
     }
@@ -70,21 +76,4 @@ const createFileAddress = function (originString, minLength, extraString) {
   return `${newString.join('')}${originString}`;
 };
 
-createFileAddress('q', 4, 'we');
-// Эта функция нам пригодится для формирования адресов файлов. Примеры её использования:
-
-// // Добавочный символ использован один раз
-// имяФункции('1', 2, '0');      // '01'
-
-// // Добавочный символ использован три раза
-// имяФункции('1', 4, '0');      // '0001'
-
-// // Добавочные символы обрезаны с конца
-// имяФункции('q', 4, 'werty');  // 'werq'
-
-// // Добавочные символы использованы полтора раза
-// имяФункции('q', 4, 'we');     // 'wweq'
-
-// // Добавочные символы не использованы, исходная строка не изменена
-// имяФункции('qwerty', 4, '0'); // 'qwerty'
-// Попробуйте не использовать при этом функцию padStart() =)
+createFileAddress('q', 9, 'wew');
